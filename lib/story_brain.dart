aa8160d64c43538561c4b827d18899668381e10a
Juliana <juliana@itsencrypted.com>
2020-01-22 11:15:29 -0300

    adding the restart(){} method and substituting 0 for _storyNumber

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -43,55 +43,98 @@ class StoryBrain {
 
   //Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData
   String getStory() {
-    return _storyData[0].storyTitle;
+    return _storyData[_storyNumber].storyTitle;
   }
 
 
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
+//Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
   //Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
   String getChoice1() {
-    return _storyData[0].choice1;
+    return _storyData[_storyNumber].choice1;
   }
   //Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
 
   String getChoice2(){
-    return _storyData[0].choice2;
+    return _storyData[_storyNumber].choice2;
   }
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
 //Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
 
- int storyNumber = 0;
-
+ int _storyNumber = 0;
 
   //Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
+  //Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
+
+  //Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
 
   void nextStory(int choiceNumber) {
-    if (choiceNumber == 1 && storyNumber == 0) {
-      storyNumber = 2;
-    } else if (choiceNumber == 2 && storyNumber == 0) {
-      storyNumber = 1;
-    } else if (choiceNumber == 1 && storyNumber == 1) {
-      storyNumber = 2;
+    //by simple if/else if
+    if (choiceNumber == 1 && _storyNumber == 0) {
+      _storyNumber = 2;
+    } else if (choiceNumber == 2 && _storyNumber == 0) {
+      _storyNumber = 1;
+    } else if (choiceNumber == 1 && _storyNumber == 1) {
+      _storyNumber = 2;
     }
-    else if (choiceNumber == 2 && storyNumber == 1) {
-      storyNumber = 3;
+    else if (choiceNumber == 2 && _storyNumber == 1) {
+      _storyNumber = 3;
     }
-    else if (choiceNumber == 1 && storyNumber == 2) {
-      storyNumber = 5;
+    else if (choiceNumber == 1 && _storyNumber == 2) {
+      _storyNumber = 5;
     }
-    else if (choiceNumber == 2 && storyNumber == 2) {
-      storyNumber = 4;
+    else if (choiceNumber == 2 && _storyNumber == 2) {
+      _storyNumber = 4;
     }
+    //Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
+
+    else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
+      restart();
+    }
+
+
+    //using case/switch
+//    switch (_storyNumber) {
+//      case 0:
+//        _storyNumber = choiceNumber == 1 ? 2 : 1;
+//        break;
+//      case 1:
+//        _storyNumber = choiceNumber == 1 ? 2 : 3;
+//        break;
+//      case 2:
+//        _storyNumber = choiceNumber == 1 ? 5 : 4;
+//        break;
+//      case 3: //End of story
+//      case 4: //End of story
+//      case 5: //End of story
+//        restart();
+//        break;
+//      default:
+//        _storyNumber = 0;
+//    }
+//
+//    //or using case switch
+//    switch (storyNumber) {
+//      case 0:
+//        if (choiceNumber == 1) {storyNumber = 2;} else {storyNumber = 1;}
+//        break;
+//      case 1:
+//        if (choiceNumber == 1) {storyNumber = 2;} else {storyNumber = 3;}
+//        break;
+//      case 2:
+//        if (choiceNumber == 1) {storyNumber = 5;} else {storyNumber = 4;}
+//        break;
+//      default:
+//        storyNumber = 0;
+//        break;
+//    }
   }
 
+  void restart(){
+    _storyNumber = 0;
+  }
 
-//Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
-
-//Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
-
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
 
