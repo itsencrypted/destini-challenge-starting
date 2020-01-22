2232c79b795392985c97e25d575687746fdffbd4
Juliana <juliana@itsencrypted.com>
2020-01-22 12:07:50 -0300

    adding the visibility Widget to main and buttonShouldBeVisible() to brain.dart

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -58,7 +58,7 @@ class StoryBrain {
   String getChoice2(){
     return _storyData[_storyNumber].choice2;
   }
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
+//Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
 //Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
 
@@ -135,7 +135,13 @@ class StoryBrain {
     _storyNumber = 0;
   }
 
-
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+//Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+  bool buttonShouldBeVisible() {
+    if (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 }
