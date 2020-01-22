dc73255551a9d186ddd14a2f91a34ea1e1a6746c
Juliana <juliana@itsencrypted.com>
2020-01-21 21:21:49 -0300

    calling methods from the brain on main.dart as button clicks, passing choice1 or choice2

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:destini_challenge_starting/story_brain.dart';
 import 'package:flutter/material.dart';
 import 'story_brain.dart';
 
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
+//Step 15 - Run the app and see if you can see the screen update with the first story.
+// Delete this if it looks as you expected.
 
 void main() => runApp(Destini());
 
@@ -56,8 +57,9 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 2,
                 child: FlatButton(
                   onPressed: () {
+                    storyBrain.nextStory(1);
                     //Choice 1 made by user.
-                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    //Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
                   },
                   color: Colors.red,
                   child: Text(
@@ -78,8 +80,9 @@ class _StoryPageState extends State<StoryPage> {
                 //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
                 child: FlatButton(
                   onPressed: () {
+                    storyBrain.nextStory(2);
                     //Choice 2 made by user.
-                    //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                    //Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
                   },
                   color: Colors.blue,
                   child: Text(
